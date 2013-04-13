1bbf803be2b298d0539236a5a5da3d5554ed7279
Chris Wade <contact@cwademusic.com>
2013-04-12 19:34:28 -0500

    minor tweaks

--- string-generator.rb
+++ string-generator.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
-
 # This program will generate a string that is the length specified as a command line argument.
 
 
-if ARGV[0] =~ /^[0-9].*/
+if ARGV[0] =~ /^[0-9]+/
 
   int = ARGV[0].to_i
   a = String.new
@@ -15,8 +14,8 @@ if ARGV[0] =~ /^[0-9].*/
   puts "Your #{a.length} char long string, prepared specially just for you:\n"
   puts a
   
-  else
-    puts <<-INFO
+else
+  puts <<-INFO
 
 Description:
   Supply an integer as an argument. A string will be generated that is the length that you specified.
@@ -26,3 +25,23 @@ Usage:
 
     INFO
 end
+
+## Another more simple version:
+#if ARGV[0] =~ /^[0-9]+/
+#
+#  a = "a" * ARGV[0].to_i
+#  
+#  puts "Your #{a.length} char long string, prepared specially just for you:\n"
+#  puts a
+#  
+#else
+#  puts <<-INFO
+#
+#Description:
+#  Supply an integer as an argument. A string will be generated that is the length that you specified.
+#
+#Usage:
+#  ruby string-generator.rb <INT>
+#
+#    INFO
+#end
